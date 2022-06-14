13de0f0337512719ab85883b7862483dbff508ba
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-14 09:53:36 +0300

    Finish setting up project

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+  end
+  attr_reader :id, :name, :age
+  attr_writer :name, :age
+
+  def can_use_services?
+    is_of_age ? true : false
+  end
+
+  private
+  def is_of_age?
+    @age >= 18 ? true : false
+   end
+  end
+end
---
+++ student.rb
@@ -0,0 +1,11 @@
+require './person'
+
+class Student < Person
+  def initialize(classroom)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
