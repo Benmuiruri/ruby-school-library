9ca478f7459d279e00117897ff27dda57afecd5c
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-14 11:34:07 +0300

    Fix constructor

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(age:, name: 'unknown', parent_permission: true)
+  def initialize(age:, name: 'unknown', parent_permission: true, classroom)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age:, name: 'unknown', parent_permission: true)
+  def initialize(age:, name: 'unknown', parent_permission: true, specialization)
     super(name: name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
