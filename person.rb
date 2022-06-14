9c1bd5faa5789e7007ccd270effd94a5a349803d
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-14 18:55:07 +0300

    Correct TrimmerDecorator

--- person.rb
+++ person.rb
@@ -48,6 +48,6 @@ end
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @component.correct_name.capitalize[0..9]
+    @component.correct_name.length > 10 ? @component.correct_name[0..9] : @component.correct_name
   end
 end
