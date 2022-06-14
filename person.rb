294c728c0065ab0e230f099a04cf37f5222c61ee
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-14 16:02:35 +0300

    Fix Rubocop

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ class Person < Nameable
   attr_reader :id, :name, :age
   attr_writer :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
