7afe52cc3795251fde431373f37d60a30fcf333a
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-14 15:27:47 +0300

    Add Decorator class

--- person.rb
+++ person.rb
@@ -27,5 +27,13 @@ class Person < Nameable
 end
 
 class Decorator < Nameable
-  
+  attr_accessor :component
+
+  def initialize(component)
+    @component = component
+  end
+
+  def correct_name
+    @component.correct_name
+  end
 end
